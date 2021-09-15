3dd409b41884b017145a4cfcbbf690df4d5015cb
Alexandr <37015618+avturin1980@users.noreply.github.com>
2021-09-15 14:59:25 +0300

    Add files via upload

---
+++ C2L1/gsapObject.js
@@ -0,0 +1,26 @@
+// let multiPass = {
+//     firstName: "Korben", 
+//     lastName: "Dallas", 
+//     age: 41, 
+//     destination: "Fhloston Paradise"
+// };
+
+// console.log(multiPass.destination);
+
+console.log(gsap.version);
+
+gsap.to("#image1", {
+    delay: 1,
+    x: 350,
+    y: 250,
+    scaleX: 4,
+    scaleY: 4
+})
+
+
+
+
+
+
+
+
---
+++ C2L6/controlMethods.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+gsap.registerPlugin(MotionPathPlugin);
+
+let timeline = gsap.timeline({
+  repeat: 2, 
+  repeatDelay: 5, 
+  defaults: {duration: 12, ease: "power1.inOut"}
+})
+
+  .to("#hand", {
+    motionPath: {
+      path:"#path", 
+      align:"#path", 
+      alignOrigin:[0.28, 0.08]}
+  })
+
+  .to("#path", {strokeDasharray: "4046, " + "0"}, "<");  
+
+document.getElementById("pause").onclick = () => timeline.pause();
+document.getElementById("play").onclick = () => timeline.play();
+document.getElementById("reverse").onclick = () => timeline.reverse();
+document.getElementById("seek").onclick = () => timeline.seek(5);
+document.getElementById("restart").onclick = () => timeline.restart();
+
+//Calculating path length:
+//console.log(document.getElementById("path").getTotalLength());
